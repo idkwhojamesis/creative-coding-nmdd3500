07a79367968f385c4fd93bbc37c8f80f9738c235
idkwhojamesis <43428092+idkwhojamesis@users.noreply.github.com>
2020-01-17 11:08:15 -0500

    Create mini-2.js

---
+++ mini-2.js
@@ -0,0 +1,35 @@
+// 4x4 grid with two circles and two squares.
+
+function setup() {
+  // Use the canvas dimensions to layout your grid.
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // Grid
+  // Column lines (x1, y1, x2, y2) at half and quarter points of canvas
+  // Set line color lol
+  stroke(0, 255, 255);
+  line(200, 0, 200, 400);
+  line(100, 0, 100, 400);
+  line(300, 0, 300, 400);
+  // Row lines the same way
+  line(0, 200, 400, 200);
+  line(0, 100, 400, 100);
+  line(0, 300, 400, 300);
+  
+  // Two circles (x, y, w, h)
+  // Set fill color
+  fill (255, 0, 0);
+  ellipse(150, 50, 50, 50);
+  ellipse(250, 50, 50, 50);
+  
+  // Two squares (x, y, w, h)
+  // Set origin point to center with rectMode()
+  rectMode(CENTER);
+  // Set color to something
+  fill(0, 255, 0);
+  rect(150, 350, 50, 50);
+  rect(250, 350, 50, 50);
+  
+}
